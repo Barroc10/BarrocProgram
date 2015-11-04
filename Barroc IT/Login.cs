669780b7b1fa9d99562aa03f06dca18c8a7156d8
JoristerStege <d208399@edu.rocwb.nl>
2015-11-04 09:14:04 +0100

    Main screen open fix

--- Barroc IT/Login.cs
+++ Barroc IT/Login.cs
@@ -49,6 +49,7 @@ namespace Barroc_IT
             correct = dbh.CountQuerry("*", "tbl_rights", "R_DepartmentPassword", password);
             if (correct == 1)
             {
+                succesful = true;
                 MainFormReference.Main.LoggedIn();
             }
             else
