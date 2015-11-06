4614571650402821349e30703ef6756cf66d0056
JoristerStege <d208399@edu.rocwb.nl>
2015-11-06 09:17:06 +0100

    create  invoice

--- Barroc IT/CreateInvoice.cs
+++ Barroc IT/CreateInvoice.cs
@@ -12,16 +12,43 @@ namespace Barroc_IT
 {
     public partial class CreateInvoice : Form
     {
+        bool error = false;
+        DatabaseHandler dbh = new DatabaseHandler();
         public CreateInvoice()
         {
             InitializeComponent();
             dtp_Date.Format = DateTimePickerFormat.Custom;
             dtp_Date.CustomFormat = "dd-MM-yyyy";
+            string[] projects = dbh.SelectQuerryAR("P_Name", "tbl_projects");
+            foreach (string item in projects)
+            {
+                if (item == null) break;
+                cb_Project.Items.Add(item);
+            }
         }
 
         private void btn_Create_Click(object sender, EventArgs e)
         {
+            checkInformation();
+            if(!error)
+            {
+                dbh.in
+            }
         }
+
+        private void checkInformation()
+        {
+                if (tb_Reason.ToString() == "")
+                {
+                    error = true;
+                }
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
 
     }
 }
